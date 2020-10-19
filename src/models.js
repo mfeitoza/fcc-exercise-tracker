f8d128acf22cc93a286553e6056d55d48eee039b
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2020-10-19 01:44:17 -0300

    fix(model)

--- src/models.js
+++ src/models.js
@@ -3,7 +3,11 @@ const { nanoid } = require('nanoid')
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
-    username: { type: String, required: true },
+    username: { 
+        type: String, 
+        required: true,
+        unique: true
+    },
     exercises: [{
         type: Schema.Types.ObjectId,
         ref: 'Exercise'
