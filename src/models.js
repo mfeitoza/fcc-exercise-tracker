96ec5e5f71717dd5df14bd7d30ecdaac3fc3db3f
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2020-10-19 01:03:59 -0300

    feat(model): add model

---
+++ src/models.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const { nanoid } = require('nanoid')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    username: { type: String, required: true },
+    exercises: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Exercise'
+    }]
+})
+
+const exerciseSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date
+    }
+})
+
+const UserModel = mongoose.model('User', userSchema)
+
+const ExerciseModel = mongoose.model('Exercise', exerciseSchema)
+
+module.exports = {
+    UserModel,
+    ExerciseModel
+}
